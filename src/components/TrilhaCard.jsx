81d7771145de6efcf5925544383f30e374e80709
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2026-04-24 12:27:06 -0300

    feat: trilha card component

---
+++ src/components/TrilhaCard.jsx
@@ -0,0 +1,32 @@
+import { motion } from 'framer-motion'
+import './TrilhaCard.css'
+
+/*
+- Para utilizar, forneça no campo "line1" a primeira linha do título e no campo "line2" a segunda linha do título 
+- O campo "icon" deve receber o nome do arquivo de ícone presente em /public/icons/trilhas/ sem extensão, como "EXEMPLO" para EXEMPLO.png
+- "type" é um campo opcional. Caso deseje utilizar um card vertical (texto abaixo do ícone), basta fornecer o valor "vertical"
+- "color" é um campo opcional para definir a cor do texto. Confira /index.css para as opções de cores ou adicione uma nova
+*/
+
+function TrilhaCard({ line1, line2, icon, type, color }) {
+
+    if (!line1 || !line2 || !icon) return null
+
+    return (
+        <motion.div 
+        className={`trilha-card ${type === 'vertical' ? 'vertical' : ''}`} 
+        whileHover={{
+            scale: 1.15,
+            transition: { duration: 0.2, ease: 'easeOut' },
+        }}
+        >
+            <img className="icon" src={`/icons/trilhas/${icon}.png`} alt={line1} />
+            <div className="trilha-label" style={{color: `var(--${color || 'yellow'})`}}>
+                <h4>{line1}</h4>
+                <h5>{line2}</h5>
+            </div>
+        </motion.div>
+    )
+}
+
+export default TrilhaCard
