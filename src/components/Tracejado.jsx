c92d761c7607e3ed13ec427bebab1ec08e5930cc
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2026-04-24 14:08:02 -0300

    feat: tracejado component

---
+++ src/components/Tracejado.jsx
@@ -0,0 +1,45 @@
+import { motion } from 'framer-motion'
+import getPathEnd from '@utils/getPathEnd'
+
+function Tracejado({d, comeco, final}) {
+    const { start, end } = getPathEnd(d);
+    return (
+        <svg className="tracejado" width="400" height="300" viewBox="0 0 400 300">
+            {final === "seta" &&
+                <marker
+                id="arrowUp"
+                markerWidth="10"
+                markerHeight="7"
+                viewBox="-2 -2 14 14"
+                refX="5"
+                refY="5"
+                orient="0"
+                markerUnits="strokeWidth"
+                >
+                    <path d="M0,10 L5,0 L10,10 Z" fill="white" />
+                </marker>
+            }
+
+            <motion.path
+                d={d}
+                fill="transparent"
+                stroke="white"
+                strokeWidth="2.5"
+                strokeDasharray="6 7"
+                initial={{ strokeDashoffset: 250 }}
+                animate={{ strokeDashoffset: 0 }}
+                transition={{
+                duration: 3,
+                ease: "easeOut",
+                }}
+                markerEnd="url(#arrowUp)"
+            />
+
+            {comeco === "circulo" &&
+                <circle cx={start.x} cy={start.y} r="6" fill="white" />
+            }
+        </svg>
+    )
+}
+
+export default Tracejado
