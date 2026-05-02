0d93b68ec178ebeb7695e9ee700dd5dbac3aba47
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2026-05-01 23:45:02 -0300

    fix: some modal svgs styles and proportions

--- src/components/mapa/CursoModal.jsx
+++ src/components/mapa/CursoModal.jsx
@@ -1,6 +1,7 @@
 import isSmallScreen from '@utils/isSmallScreen'
 import { motion } from 'framer-motion'
 import './CursoModal.css'
+import { remToPx } from '@utils/convertValues'
 import arrow from '@assets/icons/arrow-forward.svg'
 import estrelinhas1 from '@assets/enfeites/twinkles1.svg'
 import estrelinhas2 from '@assets/enfeites/twinkles2.svg'
@@ -12,12 +13,12 @@ function CursoModal({data, id}) {
         <div className="curso-modal" id={id}>
             <div className="curso-modal-title">
                 <h2 style={{ marginRight: "2rem" }}>{data.titulo}</h2>
-                <img className="estrelinhas-top" src={estrelinhas1} alt="Estrelinhas" style={{ height: '1.4rem', width: 'auto' }} />
+                <img className="estrelinhas-top" src={estrelinhas1} alt="Estrelinhas" style={{ height: '1.2rem', width: 'auto' }} />
             </div>
             <div className="curso-modal-content">
                 <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '0.5rem' }}>
                     <svg width="0.5rem" height="0.5rem" xmlns="http://www.w3.org/2000/svg">
-                        <rect x="0.1rem" y="0.1rem" width="0.3rem" height="0.3rem" transform="rotate(-45 5 5)" fill="var(--yellow)" />
+                        <rect x="0.1rem" y="0.1rem" width="0.3rem" height="0.3rem" transform={"rotate(-45 " + remToPx(0.3) + " " + remToPx(0.3) + ")"} fill="var(--yellow)" />
                     </svg>
                     <h3 style={{ color: 'var(--yellow)' }}>{data.subtitulo}</h3>
                 </div>
@@ -32,7 +33,7 @@ function CursoModal({data, id}) {
                             Acesse o curso
                             <img src={arrow} alt="->" style={{ width: '0.8rem', height: '0.8rem' }} />
                         </motion.button>
-                        <img className="estrelinhas-bottom" src={estrelinhas2} alt="Estrelinhas" style={{ height: '1.4rem', width: 'auto' }} />
+                        <img className="estrelinhas-bottom" src={estrelinhas2} alt="Estrelinhas" style={{ height: '1.2rem', width: 'auto' }} />
                     </div>
                 : null }
             </div>
